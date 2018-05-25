a75b9c146b963c9486bd8436ff48b7d77aa01b0f
Егор Пятаев <e.piataev@g.nsu.ru>
2018-05-25 17:18:09 +0700

    added russian wiki

--- problems-4/e-pyataev/lab1.py
+++ problems-4/e-pyataev/lab1.py
@@ -2,6 +2,7 @@ import time
 import urllib
 import bs4
 import requests
+import re
 
 def remove_parentheses(text):
     sharp_brackets = 0
@@ -32,51 +33,51 @@ def remove_parentheses(text):
 
     return text
 
-def find_link(search_history):
+def find_link(search_history, lang):
     response = requests.get(search_history[-1])
     html = response.text
     soup = bs4.BeautifulSoup(html, "html.parser")
 
     founded_link = None
     find = False
-    content_div = soup.find_all('div', attrs={'class' : "mw-parser-output"})
-
-    for element in content_div.find_all("p", recursive=False):
-        element = remove_parentheses(str(element))
-        element = bs4.BeautifulSoup(element, "html.parser").find('p', recursive=False)
-        for link in element.find_all("a", recursive=False):
-            print(link.get('href'))
-            classes = link.get('class') if link.get('class') is not None else []
-            if link.get('href') is None or 'new' in classes:
-                continue
-            founded_link = urllib.parse.urljoin('https://en.wikipedia.org/', link.get('href'))
-            if founded_link in search_history: continue
-            find = True
-            break
-        if find:
-            break
-    if not founded_link:
-        return
+    for content_div in soup.find_all('div', attrs={'class':'mw-parser-output'}):
+        for element in content_div.find_all("p", recursive=False):
+            element = remove_parentheses(str(element))
+            element = bs4.BeautifulSoup(element, "html.parser").find('p')
+            for link in element.find_all("a", recursive=False):
+                classes = link.get('class') if link.get('class') is not None else []
+                if link.get('href') is None or 'new' in classes:
+                    continue
+                founded_link = 'https://' + lang + '.wikipedia.org' + link.get('href')
+                find = True
+                break
+            if find:
+                break
+        if not founded_link:
+            return
 
     return founded_link
 
 def crawl(search_history, target_url):
-    if search_history[-1] == target_url:
+    if search_history[-1] in target_url:
         print("Reached the destination!")
+        return False
+    elif search_history[-1] in search_history[:-1]:
+        print("cycle!")
         return False
     else:
         return True
 
 if __name__ == "__main__":
-    start_url = input("Write link : ")
-    target_url = "https://en.wikipedia.org/wiki/Philosophy"
+    target_url = ("https://en.wikipedia.org/wiki/Philosophy", "https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F")
+    start_url = input("Write link: ")
+    lang = re.search('://(\w+).', start_url).group(1)
     links_chain = [start_url]
 
     try:
         while crawl(links_chain, target_url):
             print(urllib.parse.unquote(links_chain[-1]))
-
-            link = find_link(links_chain)
+            link = find_link(links_chain, lang)
             if not link:
                 print("No link was found!")
                 break
